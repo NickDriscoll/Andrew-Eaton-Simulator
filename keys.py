9678f47b82b23ad404b989347a1a4b2ef2f42f97
Nick Driscoll <nick.a.mavros@gmail.com>
2017-08-17 18:39:15 -0400

    Delete keys.py

--- keys.py
+++
@@ -1,6 +0,0 @@
-keys = { 
-"a" : "eGdaayM4QXTZSmGfZMCOcJm4Q",
-"b" : "j8beCvqP0HHCGgp7U5hpua5jLqT1iuL3Yn8rpZK531nyGe8AMz",
-"c" : "783790954419400704-2fNxOeLrTo4fjlEYxfqdMbK1hdtPnA3",
-"d" : "ruMsyKLwzTioy8t9D7MBlx5NbTyEKfE8NnaSjHzVP7GdN"
-}
